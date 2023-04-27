47b65b0e30687e83f656dabda6f123b09ef798d3
Matt Whiting <whitingm@usc.edu>
2023-04-26 23:15:27 -0700

    Starting Point

--- Raycast/Physics.cpp
+++ Raycast/Physics.cpp
@@ -33,25 +33,8 @@ namespace Physics {
     /// <returns>true if the LineSegment hits the Plane</returns>
     bool Plane::RayCast(const LineSegment& line, CastInfo* info) const
     {
-        Vector3 v = line.mTo - line.mFrom;
-        float d = Vector3::Dot(v, mNormal);
-        if (Math::NearZero(d))
-            return false;   // parallel
-        if (d > 0.0f)
-            return false;   // backwards
-        float t = -Vector3::Dot(line.mFrom, mNormal) - mD;
-        t = t / d;
-        if (t < 0.0f || t > 1.0f)
-            return false;
-
-        if (nullptr != info)
-        {
-            info->mNormal = mNormal;
-            info->mPoint = line.mFrom + v * t;
-            info->mFraction = t;
-        }
-
-        return true;
+        // TODO
+        return false;
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -62,7 +45,6 @@ namespace Physics {
         mPoints[0] = a;
         mPoints[1] = b;
         mPoints[2] = c;
-        GetPlane();
     }
 
     /// <summary>
@@ -71,10 +53,8 @@ namespace Physics {
     /// <returns>the normal of the Triangle</returns>
     Vector3 Triangle::GetNormal() const
     {
-        Vector3 ab = mPoints[1] - mPoints[0];
-        Vector3 ac = mPoints[2] - mPoints[0];
-        Vector3 n = Vector3::Cross(ab, ac);
-        return Vector3::Normalize(n);
+        // TODO
+        return Vector3::Zero;
     }
 
     /// <summary>
@@ -95,17 +75,7 @@ namespace Physics {
     /// <returns>true if the LineSegment hits the Triangle</returns>
     bool Triangle::RayCast(const LineSegment& line, CastInfo* info) const
     {
-        Plane p = GetPlane();
-        CastInfo tempInfo;
-        if (p.RayCast(line, &tempInfo))
-        {
-            if (IsPointInside(tempInfo.mPoint))
-            {
-                if (info)
-                    *info = tempInfo;
-                return true;
-            }
-        }
+        // TODO
         return false;
     }
 
@@ -117,33 +87,8 @@ namespace Physics {
     /// <returns>true if p is inside the triangle</returns>
     bool Triangle::IsPointInside(const Vector3& p) const
     {
-        Vector3 n = GetNormal();
-
-        {
-            Vector3 ab = mPoints[1] - mPoints[0];
-            Vector3 ap = p - mPoints[0];
-            Vector3 c = Vector3::Cross(ab, ap);
-            if (Vector3::Dot(c, n) < 0.0f)
-                return false;
-        }
-
-        {
-            Vector3 bc = mPoints[2] - mPoints[1];
-            Vector3 bp = p - mPoints[1];
-            Vector3 c = Vector3::Cross(bc, bp);
-            if (Vector3::Dot(c, n) < 0.0f)
-                return false;
-        }
-
-        {
-            Vector3 ca = mPoints[0] - mPoints[2];
-            Vector3 cp = p - mPoints[2];
-            Vector3 c = Vector3::Cross(ca, cp);
-            if (Vector3::Dot(c, n) < 0.0f)
-                return false;
-        }
-
-        return true;
+        // TODO
+        return false;
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -174,38 +119,8 @@ namespace Physics {
     /// <returns>true if the LineSegment hits the soup</returns>
     bool TriangleSoup::RayCast(const LineSegment& line, CastInfo* info) const
     {
-#ifdef TRISOUP_AABB
-#if 0
-        AABB lineAABB(line.mFrom, line.mFrom);
-        lineAABB.AddPoint(line.mTo);
-
-        if (false == mAABB.Intersect(lineAABB))
-            return false;
-#elif 0
-        if (false == mAABB.Intersect(line))
-            return false;
-#endif
-#endif
-        bool hit = false;
-        CastInfo tempInfo;
-        CastInfo bestInfo;
-        bestInfo.mFraction = 100.0f;
-
-        for (int i = 0; i < mTriCount; ++i)
-        {
-            const Triangle& tri = mTris[i];
-            if (tri.RayCast(line, &tempInfo))
-            {
-                hit = true;
-                if (tempInfo.mFraction < bestInfo.mFraction)
-                    bestInfo = tempInfo;
-            }
-        }
-        if (hit && info)
-        {
-            *info = bestInfo;
-        }
-        return hit;
+        // TODO
+        return false;
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -229,19 +144,8 @@ namespace Physics {
     /// <returns>true if the LineSegment hits the soup</returns>
     bool SoupObj::RayCast(const LineSegment& line, CastInfo* info) const
     {
-        Matrix4 world2Obj = mObj2World;
-        world2Obj.Invert();
-        LineSegment tempSegment(line);
-        tempSegment.mFrom = Vector3::Transform(tempSegment.mFrom, world2Obj);
-        tempSegment.mTo = Vector3::Transform(tempSegment.mTo, world2Obj);
-        bool ret = mSoup->RayCast(tempSegment, info);
-        if (ret && info)
-        {
-            info->mNormal = Vector3::Transform(info->mNormal, mObj2World, 0.0f);
-            info->mNormal.Normalize();
-            info->mPoint = Vector3::Transform(info->mPoint, mObj2World, 1.0f);
-        }
-        return ret;
+        // TODO
+        return false;
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -253,6 +157,11 @@ namespace Physics {
     World::~World()
     {}
 
+    /// <summary>
+    /// Add an object to the world.
+    /// Feel free to edit this function if you want to
+    /// </summary>
+    /// <param name="obj">the object being added</param>
     void World::AddObj(const SoupObj& obj)
     {
         mObj.push_back(obj);
@@ -267,24 +176,7 @@ namespace Physics {
     /// <returns>true if the LineSegment hits the anything in the World</returns>
     bool World::RayCast(const LineSegment& line, CastInfo* info) const
     {
-        bool hit = false;
-        CastInfo tempInfo;
-        CastInfo bestInfo;
-        bestInfo.mFraction = 100.0f;
-
-        for (const SoupObj& obj : mObj)
-        {
-            if (obj.RayCast(line, &tempInfo))
-            {
-                hit = true;
-                if (tempInfo.mFraction < bestInfo.mFraction)
-                    bestInfo = tempInfo;
-            }
-        }
-        if (hit && info)
-        {
-            *info = bestInfo;
-        }
-        return hit;
+        // TODO
+        return false;
     }
 }
